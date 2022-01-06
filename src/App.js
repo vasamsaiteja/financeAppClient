20dc84c4f033bae48aa424f5bc3b88d6fc934cc8
vassamsai <vasamsettisaiteja68@gmail.com>
2022-01-06 09:36:17 +0000

    updatedCommit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-// import {Route} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import LoginForm from './components/LoginForm'
-// import Home from './components/Home'
+import Home from './components/Home'
 
 import './App.css'
 
 const App = () => (
-  <>
-    {/* <Route exact path="/login" component={LoginForm} />
-    <Route exact path="/" component={Home} /> */}
-    {/* <Route component={NotFound} /> */}
-    <LoginForm />
-  </>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/login" component={LoginForm} />
+      <Route exact path="/" component={Home} />
+      {/* <Route component={NotFound} /> */}
+      {/* <LoginForm /> */}
+    </Switch>
+  </BrowserRouter>
 )
 
 export default App
--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -32,10 +32,13 @@ class LoginForm extends Component {
     event.preventDefault()
     const {username, password} = this.state
     const userDetails = {username, password}
-    const url = 'https://apis.ccbp.in/login'
+    const url = 'https://vassam-sai-financepeer.herokuapp.com/login'
     const options = {
       method: 'POST',
       body: JSON.stringify(userDetails),
+      headers: {
+        'Content-Type': 'application/json',
+      },
     }
     const response = await fetch(url, options)
     const data = await response.json()
